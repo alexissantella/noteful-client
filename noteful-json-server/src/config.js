7da15a65de7fd547ade452bc03e05605618e20f9
Alexis Santella <69594663+alexissantella@users.noreply.github.com>
2021-07-12 14:38:18 -0400

    Add files via upload

--- noteful-json-server/src/config.js
+++ noteful-json-server/src/config.js
@@ -1,3 +1,6 @@
-export default {
-    API_ENDPOINT: 'https://gentle-lake-89595.herokuapp.com/'
+module.exports = {
+    PORT: process.env.PORT || 9090,
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/blogful',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/blogful-test',
+    NODE_ENV: process.env.NODE_ENV || 'development',
   }
